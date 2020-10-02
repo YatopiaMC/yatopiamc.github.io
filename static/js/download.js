6fe388e05b291d01b9f56c3c0ee30a59c4a619b3
Mykyta Komarn <nkomarn@hotmail.com>
2020-10-02 11:46:10 -0700

    Make the download button actually work

--- static/js/download.js
+++ static/js/download.js
@@ -10,7 +10,7 @@ request.onload = () => {
         let build = builds[i];
 
         let row = table.insertRow(-1);
-        row.insertCell(0).innerHTML = `<a class="download-button">#${build['number']} <span class="material-icons">get_app</span></a>`;
+        row.insertCell(0).innerHTML = `<a class="download-button" href="https://ci.codemc.io/job/YatopiaMC/job/Yatopia/job/ver%2F1.16.3/${build['number']}/artifact/target/yatopia-1.16.3-paperclip-b${build['number']}.jar">#${build['number']} <span class="material-icons">get_app</span></a>`;
         row.insertCell(1).innerHTML = `${build['branch']['commit']['message']} [<a href="https://github.com/YatopiaMC/Yatopia/commit/${build['branch']['commit']['sha']}">commit</a>]`;
         row.insertCell(2).innerHTML = `${build['branch']['commit']['authoredAt'].split(" ")[0]}`;
     }
