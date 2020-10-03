9caec113a006b01fdf29656fc9ceee6056b5ce31
Ivan Pekov <ivan@mrivanplays.com>
2020-10-03 08:36:42 +0300

    Dynamic version parsing and stable build

--- static/js/download.js
+++ static/js/download.js
@@ -1,13 +1,27 @@
 let table = document.getElementById("table");
 
+const STABLE_BUILD = 90;
+// we'll show it later
+document.getElementById('stable').style.display = 'none'
+
 var request = new XMLHttpRequest();
 request.open("GET", "https://api.yatopia.net/v2/builds", true);
 request.onload = () => {
     console.log(JSON.parse(request.responseText))
+    let jsonResponse =  JSON.parse(request.responseText);
+    if (jsonResponse['error'] != null) {
+        document.getElementById('error').style.display = 'block'
+        return;
+    }
     let builds = JSON.parse(request.responseText)['builds'];
+    var version = builds[0]['branch']['name'].replace('ver/', '');
+    document.getElementById('buildsAre').innerText = "These builds are for Minecraft " + version + ".";
 
     for (var i = 0; i < builds.length; i++) {
         let build = builds[i];
+        if (build['number'] === STABLE_BUILD) {
+            document.getElementById('stable').outerHTML = `<a id="stable" class="download-button" href="${build['downloadUrl']}">Latest stable build: #${build['number']}<span class="material-icons">get_app</span></a>`
+        }
 
         let row = table.insertRow(-1);
         row.insertCell(0).innerHTML = `<a class="download-button" href="${build['downloadUrl']}">#${build['number']} <span class="material-icons">get_app</span></a>`;
