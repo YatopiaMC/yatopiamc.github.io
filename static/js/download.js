715cad9fdad787b2dd68707175c820e327ca17a0
Ivan Pekov <ivan@mrivanplays.com>
2020-10-04 13:42:06 +0300

    Combine commit messages from failed build(s) into the last successful
    Say hello to Ivan, the js god.

--- static/js/download.js
+++ static/js/download.js
@@ -7,7 +7,6 @@ document.getElementById('stable').style.display = 'none'
 var request = new XMLHttpRequest();
 request.open("GET", "https://api.yatopia.net/v2/builds", true);
 request.onload = () => {
-    console.log(JSON.parse(request.responseText))
     let jsonResponse =  JSON.parse(request.responseText);
     if (jsonResponse['error'] != null) {
         document.getElementById('error').style.display = 'block'
@@ -20,14 +19,50 @@ request.onload = () => {
 
     for (var i = 0; i < builds.length; i++) {
         let build = builds[i];
+        if (build['status'] === "FAILURE" || build['status'] === "BUILDING") {
+            continue;
+        }
         if (build['number'] === STABLE_BUILD) {
             document.getElementById('stable').outerHTML = `<a id="stable" class="download-button" href="${build['downloadUrl']}">Latest stable build: #${build['number']}<span class="material-icons">get_app</span></a>`
+        }
+        var commitMessage = "";
+        var dateMessage = "";
+        let changeSets = build['changeSets'];
+        for (var i1 = 0; i1 < changeSets.length; i1++) {
+            let changeSet = changeSets[i1];
+            commitMessage += changeSet['message'] + ` [<a href="https://github.com/YatopiaMC/Yatopia/commit/${changeSet['sha']}">commit</a>]<br>`;
+            dateMessage += changeSet['authoredAt'].split(" ")[0] + "<br>";
+        }
+        if ((i + 1 < builds.length) && builds[i + 1]['status'] === "FAILURE") {
+            let messages = getFailureBuildsUntilSuccessfulFromLatest(builds, i + 1).split("\00");
+            commitMessage += messages[0];
+            dateMessage += messages[1];
         }
 
         let row = table.insertRow(-1);
         row.insertCell(0).innerHTML = `<a class="download-button" href="${build['downloadUrl']}">#${build['number']} <span class="material-icons">get_app</span></a>`;
-        row.insertCell(1).innerHTML = `${build['branch']['commit']['message']} [<a href="https://github.com/YatopiaMC/Yatopia/commit/${build['branch']['commit']['sha']}">commit</a>]`;
-        row.insertCell(2).innerHTML = `${build['branch']['commit']['authoredAt'].split(" ")[0]}`;
+        row.insertCell(1).innerHTML = commitMessage;
+        row.insertCell(2).innerHTML = dateMessage;
     }
 }
 request.send(null)
+
+function getFailureBuildsUntilSuccessfulFromLatest(builds, from) {
+    var commitMessage = "";
+    var dateMessage = "";
+    for (var i = from; i < builds.length; i++) {
+        let build = builds[i];
+        let changeSets = build['changeSets'];
+        if (build['status'] === "FAILURE") {
+            for (var i1 = 0; i1 < changeSets.length; i1++) {
+                let changeSet = changeSets[i1];
+                commitMessage += changeSet['message'] + ` [<a href="https://github.com/YatopiaMC/Yatopia/commit/${changeSet['sha']}">commit</a>]<br>`;
+                dateMessage += changeSet['authoredAt'].split(" ")[0] + "<br>";
+            }
+        }
+        if (build['status'] === "SUCCESS" && i !== from) {
+            break;
+        }
+    }
+    return commitMessage + "\00" + dateMessage;
+}
